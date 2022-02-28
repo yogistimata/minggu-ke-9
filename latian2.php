d2a96486c009b439d7fa51b8c038993e5978aa80
yogistimata <sasmitayogi8@gmail.com>
2022-03-01 06:04:11 +0700

    latian 2

---
+++ latian2.php
@@ -0,0 +1,34 @@
+<?php
+$mahasiswa= [
+    ["Nama" => "Yogi Sasmita" ,
+    "NIM" => "21520007" ,
+    "Jurusan" =>  "Teknologi Informasi",
+    "Gambar" => "Yogi.jpeg"
+    ],
+    
+    ["Nama" => "Lia" ,
+    "NIM" => "21510007" ,
+    "Jurusan" => "Sistem Informasi",
+    "Gambar" => "Lia.jpeg"
+    ]
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latian get</title>
+</head>
+<body>
+ <h1>Data mahasiswa</h1>
+<ul>
+ <?php foreach ($mahasiswa as $mhs):?>
+    <li>
+        <a href= "latian3.php"><?= $mhs ["Nama"]; ?></a>
+    </li>
+<?php endforeach; ?>
+ </ul> 
+</body>
+</html>
