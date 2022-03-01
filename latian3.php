0c6756a37530b97e656e1809e13eec5c9c243275
yogistimata <sasmitayogi8@gmail.com>
2022-03-01 13:51:08 +0700

    latian 3

---
+++ latian3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data mahasiswa</title>
+</head>
+<body>
+<ul>
+    <li><img src="img/<?= $_GET["Gambar"]; ?>" ></li>
+    <li><?= $_GET["Nama"]; ?></li>
+    <li><?= $_GET["NIM"]; ?></li>
+    <li><?= $_GET["Jurusan"]; ?></li>
+</ul>
+<a href= "latian2.php">Back To Menu </a>
+</body>
+</html>
